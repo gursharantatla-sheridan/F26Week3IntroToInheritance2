3eb35fcca8534de93edb389c874793dc9f8bbe80
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-21 14:35:21 -0400

    comm emp class

---
+++ F26Week3IntroToInheritance/BaseClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F26Week3IntroToInheritance
+{
+    public class BaseClass
+    {
+        private int myPrivateVar;
+        protected int myProtectedVar;
+        public int myPublicVar;
+
+        public BaseClass()
+        {
+            Console.WriteLine("Base class constructor called");
+        }
+
+        public BaseClass(string msg)
+        {
+            Console.WriteLine("Base class constructor with message: " + msg);
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public DerivedClass(string msg) : base(msg)
+        {
+            Console.WriteLine("Derived class constructor called");
+        }
+
+        public void MyMethod()
+        {
+            //myPrivateVar = 1;
+            myProtectedVar = 2;
+            myPublicVar = 3;
+        }
+    }
+}
--- F26Week3IntroToInheritance/Program.cs
+++ F26Week3IntroToInheritance/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //BaseClass objBase = new BaseClass();
+            //objBase.myProtectedVar = 1;
+            //objBase.myPublicVar = 1;
+
+            DerivedClass objDerived = new DerivedClass("hello");
+            ////objDerived.myProtectedVar = 1;
+            //objDerived.myPublicVar = 2;
         }
     }
 }
